8d714bb9d1065d9073ffee3a2466b6b67902ce61
= <sharukhprogramming@gmail.com>
2025-06-01 00:06:46 +0530

    added delete address controller

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sharukh010/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddress() gin.HandlerFunc{
 
@@ -15,5 +24,31 @@ func EditWorkAddress() gin.HandlerFunc{
 }
 
 func DeleteAddress() gin.HandlerFunc{
-	
+	return func(c *gin.Context){
+		user_id := c.Query("id")
+		if user_id == ""{
+			c.JSON(http.StatusBadRequest,gin.H{"error":"User ID required"})
+			c.Abort()
+			return 
+		}
+		addresses := make([]models.Address,0)
+		user_ID,err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError,"Internal Server Error")
+			return 
+		}
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key:"_id",Value:user_ID}}
+		update := bson.D{{Key:"$set",Value:bson.D{primitive.E{Key:"address",Value:addresses}}}}
+
+		_,err = UserCollection.UpdateOne(ctx,filter,update)
+
+		if err != nil {
+			c.IndentedJSON(http.StatusNotFound,"User not found with given ID")
+			return 
+		}
+		c.IndentedJSON(http.StatusOK,"Successfully Deleted Address")
+
+	}
 }
