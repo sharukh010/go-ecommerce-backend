9ca3dbf9053fbb54d006124a6e83ec0968d8ea86
= <sharukhprogramming@gmail.com>
2025-06-02 21:50:53 +0530

    added get Items From Cart controller

--- controllers/cart.go
+++ controllers/cart.go
@@ -3,11 +3,14 @@ package controllers
 import (
 	"context"
 	"errors"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sharukh010/go-ecommerce/database"
+	"github.com/sharukh010/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -90,7 +93,48 @@ func (app *Application) RemoveItem() gin.HandlerFunc{
 }
 
 func (app *Application) GetItemFromCart() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		userQueryID := c.Query("id")
+		if userQueryID == ""{
+			c.JSON(http.StatusBadRequest,ErrUserIDRequired.Error())
+			return 
+		}
+		userID,err := primitive.ObjectIDFromHex(userQueryID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError,err.Error())
+			return 
+		}
 
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		defer cancel()
+
+		var filledCart models.User
+		err = app.userCollection.FindOne(ctx,bson.M{"_id":userID}).Decode(&filledCart)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError,err.Error())
+			return 
+		}
+		filter_match := bson.D{{Key:"$match",Value:bson.D{primitive.E{Key:"_id",Value: userID}}}}
+		unwind := bson.D{{Key:"$unwind",Value:bson.D{primitive.E{Key:"path",Value: "$usercart"}}}}
+		grouping := bson.D{{Key:"$group",Value:bson.D{primitive.E{Key:"_id",Value: "$_id"},{Key:"total",Value:bson.D{primitive.E{Key:"$sum",Value:"$usercart.price"}}}}}}
+		
+		pointCursor,err := UserCollection.Aggregate(ctx,mongo.Pipeline{filter_match,unwind,grouping})
+
+		if err != nil {
+			log.Println(err.Error())
+		}
+		var listing []bson.M 
+		if err = pointCursor.All(ctx,&listing); err != nil {
+			log.Println(err.Error())
+			c.JSON(http.StatusInternalServerError,"Error occured while fetching cart items")
+		}
+
+		for _,json := range listing{
+			c.JSON(http.StatusOK,json["total"])
+			c.JSON(http.StatusOK,filledCart.UserCart)
+		}
+
+	}
 }
 
 func (app *Application) BuyFromCart() gin.HandlerFunc{
